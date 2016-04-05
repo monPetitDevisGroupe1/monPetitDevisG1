10300664eee99b5ede4a1fe519a7fbb8456f55ca
Pierre Le Marre <pierrelemarre@gmail.com>
2016-04-05 16:32:09 +0100

    Dahsboard JDBC

--- spring-boot-sample-tomcat-jsp/src/main/java/sample/tomcat/jsp/DashboardController.java
+++ spring-boot-sample-tomcat-jsp/src/main/java/sample/tomcat/jsp/DashboardController.java
@@ -1,31 +1,28 @@
 package sample.tomcat.jsp;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Date;
 import java.util.Map;
 
 /**
  * Created by Pierre on 05/04/2016.
  */
+@Controller
 public class DashboardController {
 
     @Autowired
     DataSource dataSource;
 
 
-    @RequestMapping({"/","/index"})
-    public String welcome(Map<String, Object> model) {
-
-
-/* Connexion à la base de données */
-        String url = "jdbc:mysql://localhost:3306/bdd_sdzee";
-
+    @RequestMapping({"/dashboard"})
+    public String dashboard(Map<String, Object> model) {
+        Statement statement = null;
+        ResultSet resultat = null;
         Connection connexion = null;
         try {
             connexion = dataSource.getConnection();
@@ -38,14 +35,22 @@ public class DashboardController {
         } finally {
             if ( connexion != null )
                 try {
+
+                    statement = connexion.createStatement();
+                    resultat = statement.executeQuery("SELECT * FROM devis WHERE etape < 4 ORDER BY id_devis DESC LIMIT 1 ");
+                    while(resultat.next()){
+                        model.put("nom", resultat.getString("nom"));
+                        model.put("type_devis", resultat.getString("type_devis"));
+                    }
             /* Fermeture de la connexion */
                     connexion.close();
                 } catch ( SQLException ignore ) {
             /* Si une erreur survient lors de la fermeture, il suffit de l'ignorer. */
                 }
-        }
 
-        model.put("time", new Date());
-        return "welcome";
+
+
+        }
+        return "dashboard";
     }
 }
